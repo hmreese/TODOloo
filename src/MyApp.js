0a2c1b10e9d613209f636292d17be16d24d5fdbe
hmreese <hmreese308@gmail.com>
2021-10-31 14:17:29 -0700

    database connected

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,81 +1,71 @@
-import React, {useState, useEffect} from 'react';
-import Table from './Table';
-import Form from './Form';
-import axios from 'axios';
-
-
-
+import React, { useState, useEffect } from "react";
+import Table from "./Table";
+import Form from "./Form";
+import axios from "axios";
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
 
-  function removeOneCharacter (index) {
+  function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
-      return i !== index
+      return i !== index;
     });
-    deleteid(characters[index])
+    deleteid(characters[index]);
     setCharacters(updated);
-
   }
 
   useEffect(() => {
-    fetchAll().then( result => {
-      if (result)
-        setCharacters(result);
+    fetchAll().then((result) => {
+      if (result) setCharacters(result);
     });
-  }, [] );
+  }, []);
 
-  async function makePostCall(person){
+  async function makePostCall(person) {
     try {
-       const response = await axios.post('http://localhost:5000/users', person);
-       return response.data;
-    }
-    catch (error) {
-       console.log(error);
-       return false;
+      const response = await axios.post("http://localhost:5000/users", person);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      return false;
     }
   }
 
-  async function fetchAll(){
+  async function fetchAll() {
     try {
-       const response = await axios.get('http://localhost:5000/users');
-       return response.data.users_list;
-    }
-    catch (error){
-       //We're not handling errors. Just logging into the console.
-       console.log(error);
-       return false;
-    }
- }
-
-  async function deleteid(person){
-    try {
-
-      const response = await axios.delete('http://localhost:5000/users/'.concat(person._id));
-      return response.data;
-    }
-    catch (error){
+      const response = await axios.get("http://localhost:5000/users");
+      return response.data.users_list;
+    } catch (error) {
       //We're not handling errors. Just logging into the console.
       console.log(error);
       return false;
     }
   }
 
- function updateList(person) {
-  makePostCall(person).then( result => {
-  if (result)
-     setCharacters([...characters, result] );
-  });
-}
+  async function deleteid(person) {
+    try {
+      const response = await axios.delete(
+        "http://localhost:5000/users/".concat(person._id)
+      );
+      return response.data;
+    } catch (error) {
+      //We're not handling errors. Just logging into the console.
+      console.log(error);
+      return false;
+    }
+  }
 
+  function updateList(person) {
+    makePostCall(person).then((result) => {
+      if (result) setCharacters([...characters, result]);
+    });
+  }
 
   return (
     <div className="container">
       <Table characterData={characters} removeCharacter={removeOneCharacter} />
       <Form handleSubmit={updateList} />
     </div>
-  )
+  );
 }
-
 
 export default MyApp;
