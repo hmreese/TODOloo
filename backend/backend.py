d9238f43d7289389adfeabeb68c68f3b44caec52
mickreb56 <56707357+mickreb56@users.noreply.github.com>
2021-11-01 12:48:01 -0700

    Update backend.py

--- backend/backend.py
+++ backend/backend.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import request
 from flask import jsonify
+import hashlib
 import json
 from flask_cors import CORS
 from mongodb import User
@@ -45,10 +46,59 @@ CORS(app)
 # }
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, World!'
 
+
+@app.route('/', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        ret = request.get_json()
+
+        try:
+            username = ret["username"]
+            password = ret["password"]
+        except:
+            return jsonify({}), 400
+
+        if password is None or username is None:
+            return jsonify({}), 400
+        hashedPas = hashlib.sha256(password.encode())
+
+        # TODO check database for username and pass
+        # if pass != databasePass:
+        # return jsonify({"username":username}),400
+
+        # return the object key for user
+        return jsonify({"username": username}), 200
+
+
+@app.route('/api/users', methods=['POST'])
+def create_user():
+    # name
+    # username
+    # password
+    if request.method == 'POST':
+        ret = request.get_json()
+
+        try:
+            username = ret["username"]
+            password = ret["password"]
+            name = ret["name"]
+        except:
+            return jsonify({}), 418
+
+        if password is None or username is None:
+            return jsonify({}), 418
+
+        hashedPas = hashlib.sha256(password.encode())
+
+        # TODO add user data to database
+        # if unsuccessful
+        # return jsonify({"username":username}),400
+
+        return jsonify({"username": username}), 201
 
 @app.route('/<id>/home')
 def get_home(id):
